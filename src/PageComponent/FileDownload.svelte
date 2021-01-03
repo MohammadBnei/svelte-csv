57bfceb323fbd9086bf368c54021fff0fea7233e
mohammadbnei <mohammadamine.banaei@gmail.com>
2021-01-03 18:43:26 +0100

    Download management

--- src/PageComponent/FileDownload.svelte
+++ src/PageComponent/FileDownload.svelte
@@ -4,8 +4,33 @@
 
     const downloadCSV = async () => {
         loader.loading();
-        const csv = await new Promise((resolve, reject) => {
-            const csv = Papa.unparse($csvStore.rows, $csvStore.meta);
+        const csv = await new Promise((resolve) => {
+            const rows = $csvStore.rows.map((row) => {
+                /**
+                 * Since the column name remapping doe not impact the display name,
+                 * we need to address that before proceding to the unparsing
+                */
+                for (const key of Object.keys(row)) {
+                    const column = $csvStore.columns.find(
+                        ({ dataName }) => dataName === key
+                    );
+                    if (column?.display !== column.dataName) {
+                        row[column.display] = row[key];
+                        delete row[key];
+                    }
+                }
+
+                return row;
+            });
+
+            /**
+             * Used to maintain the column order
+             */
+            const columns = $csvStore.columns.reduce((acc, cur) => {
+                acc.push(cur.display);
+                return acc;
+            }, []);
+            const csv = Papa.unparse(rows, { ...$csvStore.meta, columns });
 
             resolve(csv);
         });
